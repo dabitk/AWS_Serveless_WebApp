f689da7f3af52406e4d5b981148a4208ba33912d
dabitk <dabitk@gmail.com>
2020-02-05 20:34:47 +0900

    first commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-northeast-2_r48bzV6Dk', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1tbtr4bfhfknd99p6kcjplap70', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://7n4k30rhsa.execute-api.ap-northeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
